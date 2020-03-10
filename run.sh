194e24fed5f92e51d5f97e160794aa40e86d4f1b
ruslan.smirnoff <vedmedka@gmail.com>
2020-03-10 13:36:08 +0200

    add auth/credentials_file_override

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -x #echo on
 
 gcloud info --run-diagnostics
 
 gcloud app versions list --project $GOOGLE_CLOUD_PROJECT
 
-gcloud app deploy app.yaml --project $GOOGLE_CLOUD_PROJECT --verbosity=debug
+gcloud config set auth/credential_file_override $GOOGLE_APPLICATION_CREDENTIALS
+
+gcloud app deploy app.yaml --project $GOOGLE_CLOUD_PROJECT 
